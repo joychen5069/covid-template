33ce9d2974d787611289585014b86ff0db61066a
Joy Chen <joychen5069@gmail.com>
2020-12-14 10:08:47 -0500

    fixed so state shows up full name, not just abbreviation. and fixed decimal point in new cases

--- client/src/components/members/StateSearch.jsx
+++ client/src/components/members/StateSearch.jsx
@@ -22,12 +22,68 @@ export default class StateSearch extends React.Component {
         province
     );
     console.log(res.data[0]);
-    this.setState({
-      state: res.data[0].state,
-      deaths: res.data[0].tot_death,
-      new: res.data[0].new_case,
-      total: res.data[0].tot_cases,
-    });
+    let states =[
+      ['Arizona', 'AZ'],
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawaii', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Mississippi', 'MS'],
+      ['Missouri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['New Hampshire', 'NH'],
+      ['New Jersey', 'NJ'],
+      ['New Mexico', 'NM'],
+      ['New York', 'NY'],
+      ['North Carolina', 'NC'],
+      ['North Dakota', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregon', 'OR'],
+      ['Pennsylvania', 'PA'],
+      ['Rhode Island', 'RI'],
+      ['South Carolina', 'SC'],
+      ['South Dakota', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY'],
+    ]
+    for (var i = 0; i < states.length; i++) {
+      if (states[i][1] === res.data[0].state) {
+        this.setState({
+          state: states[i][0],
+          deaths: res.data[0].tot_death,
+          new: Math.floor(res.data[0].new_case),
+          total: res.data[0].tot_cases,
+        });
+      }}
+    
   }
 
   provinceCallback = (province) => {
@@ -45,7 +101,7 @@ export default class StateSearch extends React.Component {
         </div>
         <div className="col-sm-12 stateApiCallWrapper">
           <h5 className="stateDataText">
-            State: <span className="stateName">{this.state.province}</span>
+            State: <span className="stateName">{this.state.state}</span>
           </h5>
           <div className="countyDataWrapper">
             <h5>
